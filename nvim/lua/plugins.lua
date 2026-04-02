d2fafa1838a7f4d768d12ad5fa39f6091b14b2dc
bryam <nunezb@uni-greifswald.de>
2026-04-02 09:57:06 +0200

    removing mason for native lsp support

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,30 +5,25 @@ vim.call('plug#begin')
 
 -- Basics
 Plug('Mofiqul/vscode.nvim', { ['as'] = 'vscode' })
-Plug('ggandor/leap.nvim')
-Plug('akinsho/toggleterm.nvim', { ['tag'] = '*' })
-Plug('windwp/nvim-autopairs')
+--Plug('ggandor/leap.nvim')
+--Plug('akinsho/toggleterm.nvim', { ['tag'] = '*' })
+--Plug('windwp/nvim-autopairs')
 
 -- Syntax highlighting
-Plug("nvim-treesitter/nvim-treesitter", { ['do'] = ":TSUpdate" })
-Plug("nvim-treesitter/nvim-treesitter-context")
-Plug("nvim-treesitter/nvim-treesitter-textobjects")
-
--- LSP
-Plug("mason-org/mason.nvim")
-Plug("neovim/nvim-lspconfig")
-Plug("mason-org/mason-lspconfig.nvim")
+--Plug("nvim-treesitter/nvim-treesitter", { ['do'] = ":TSUpdate" })
+--Plug("nvim-treesitter/nvim-treesitter-context")
+--Plug("nvim-treesitter/nvim-treesitter-textobjects")
 
 -- Linting and Formating
-Plug('mfussenegger/nvim-lint')
-Plug("stevearc/conform.nvim")
+--Plug('mfussenegger/nvim-lint')
+--Plug("stevearc/conform.nvim")
 
 -- Telescope
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim')
+--Plug('nvim-lua/plenary.nvim')
+--Plug('nvim-telescope/telescope.nvim')
 
 -- Harpoon
-Plug('ThePrimeagen/harpoon', { ['branch'] = 'harpoon2' })
+--Plug('ThePrimeagen/harpoon', { ['branch'] = 'harpoon2' })
 
 -- End of Plugins
 vim.call('plug#end')
