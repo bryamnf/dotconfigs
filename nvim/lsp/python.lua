782d79c68fd1a7a55e391d9a02f3c20d6f0fada2
bryam <nunezb@uni-greifswald.de>
2026-04-07 09:16:25 +0200

    lsp's

---
+++ nvim/lsp/python.lua
@@ -0,0 +1,13 @@
+return {
+  cmd = { "basedpyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_markers = { "pyrightconfig.json", "pyproject.toml", "setup.py",  "requirements.txt", ".git" },
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+      },
+    },
+  }
+}
