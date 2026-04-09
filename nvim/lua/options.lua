ad8339c363630dc8a2efd401fb7831118bebe8d3
bryam <nunezb@uni-greifswald.de>
2026-04-09 13:03:45 +0200

    better to go with 4 spaces since it's the standar of vanilla vim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -16,9 +16,9 @@ vim.loader.enable()
 
 
 -- Set tab and indentation to 2 spaces
-vim.opt.tabstop = 2        -- Number of visual spaces per TAB
-vim.opt.softtabstop = 2    -- Number of spaces in tab when editing
-vim.opt.shiftwidth = 2     -- Number of spaces for each indentation
+vim.opt.tabstop = 4        -- Number of visual spaces per TAB
+vim.opt.softtabstop = 4    -- Number of spaces in tab when editing
+vim.opt.shiftwidth = 4     -- Number of spaces for each indentation
 vim.opt.expandtab = true   -- Use spaces instead of tabs
 vim.opt.smartindent = true -- Autoindent new lines
 vim.opt.autoindent = true  -- Copy indent from current line when starting a new one
