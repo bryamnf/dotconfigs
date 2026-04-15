8013920beac1ffdd7d849fec499c7483bf48b553
bryam <nunezb@uni-greifswald.de>
2026-04-15 22:34:07 +0200

    added more default parsers and switch to mini-icons

--- nvim/plugin/nvim-treesitter.lua
+++ nvim/plugin/nvim-treesitter.lua
@@ -1,3 +1,4 @@
 vim.pack.add{
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
 }
+require('nvim-treesitter').install { 'python', 'bash', 'typescript', 'markdown', 'markdown_inline', 'html', 'yaml', 'latex'}
--- nvim/plugin/nvim-web-devicons.lua
+++
@@ -1,4 +0,0 @@
-vim.pack.add{
-    { src = 'https://github.com/nvim-tree/nvim-web-devicons.git' },
-}
-require("nvim-web-devicons").setup()
