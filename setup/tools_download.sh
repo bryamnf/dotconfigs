61b9772eff5166191ad2fc04c109eee478189adc
bryam <nunezb@uni-greifswald.de>
2026-04-15 22:52:17 +0200

    is more like dev tools_download

---
+++ setup/tools_download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+pixi global install lua-language-server
+uv tool install basedpyright
+uv tool install pylatexenc #markdown render pkg
+cargo install --locked tree-sitter-cli
+npm install -g bash-language-server
+npm install -g libtexprintf #markdown render pkg
