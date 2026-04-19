c5e6945bcf5da3328ff142ed456c61aa0eea4e86
bryam <nunezb@uni-greifswald.de>
2026-04-19 15:52:07 +0200

    needed for incremental selection

--- nvim/plugin/flash.lua
+++ nvim/plugin/flash.lua
@@ -266,3 +266,11 @@ vim.keymap.set({'n', 'x', 'o'}, '<leader>s', function() require("flash").treesit
 vim.keymap.set('o', 'r', function() require("flash").remote() end, { desc = "Remote Flash" })
 vim.keymap.set({'n','o', 'x'}, '<leader>t', function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
 vim.keymap.set('c', '<C-S-c>', function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
+vim.keymap.set({"n", "x", "o"}, "<c-space>", function()
+  require("flash").treesitter({
+    actions = {
+      ["<c-space>"] = "next",
+      ["<BS>"] = "prev"
+    }
+  })
+end, { desc = "Treesitter incremental selection" })
