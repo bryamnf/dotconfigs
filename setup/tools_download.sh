36d93fa20ef078ab86829101dca39ca526111cfe
bryam <nunezb@uni-greifswald.de>
2026-04-22 10:36:03 +0200

    zoxide
    zoxide integration in the bashrc (checking that is there after checking
    cargo) and also downloading it with cargo in bash tools_dowload.sh

--- setup/tools_download.sh
+++ setup/tools_download.sh
@@ -4,5 +4,6 @@ uv tool install basedpyright
 uv tool install ruff
 uv tool install pylatexenc #markdown render pkg
 cargo install --locked tree-sitter-cli
+cargo install --locked zoxide
 npm install -g bash-language-server
 npm install -g libtexprintf #markdown render pkg
