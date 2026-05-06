6a44eadd6561e0fb870ca20a97b2c1e09a95264a
bryam <nunezb@uni-greifswald.de>
2026-05-06 08:41:25 +0200

    ts lsp

---
+++ nvim/lsp/vtsls.lua
@@ -0,0 +1,8 @@
+return {
+  cmd = {"vtsls", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  root_markers = {  'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', 'deno.json', 'deno.jsonc','deno.lock',".git" },
+  init_options = {
+      hostInfo = "neovim"
+  }
+}
