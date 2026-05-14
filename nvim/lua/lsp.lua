964163c866d0739c98f6fb13c757036ed4542732
bryam <nunezb@uni-greifswald.de>
2026-05-14 13:55:16 +0200

    they are not needed since fzf-lua takes care of this

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -28,19 +28,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end
 
 
-        -- ==========================================================================
-        -- 2. IMPACT ANALYSIS & REFERENCE NAVIGATION
-        -- ==========================================================================
-
         if client:supports_method('textDocument/references') then
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        end
-
-        -- Call Hierarchy
-        if client:supports_method('textDocument/prepareCallHierarchy') then
-            vim.keymap.set('n', 'gh', function()
-                vim.lsp.buf.call_hierarchy()
-            end, opts)
         end
 
         -- Reference Navigation (Quickfix list movement)
@@ -50,51 +39,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<A-n>', ':cnext<CR>', opts)
         vim.keymap.set('n', '<A-p>', ':cprev<CR>', opts)
 
-
-        -- ==========================================================================
-        -- 3. BIRD'S EYE VIEW & CONTEXT
-        -- ==========================================================================
-
-        -- Symbols
-        if client:supports_method('textDocument/documentSymbol') then
-            vim.keymap.set('n', '<leader>ds', vim.lsp.buf.document_symbol, opts)
-        end
-
-        if client:supports_method('workspace/symbol') then
-            vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, opts)
-        end
-
-        -- Hover & Signature Help
-        if client:supports_method('textDocument/hover') then
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        end
-
-        if client:supports_method('textDocument/signatureHelp') then
-            vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, opts)
-            vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
-        end
-
-
-        -- ==========================================================================
-        -- 4. CODE MODIFICATIONS & ACTIONS
-        -- ==========================================================================
-
-        -- Code Actions & Source Actions
-        if client:supports_method('textDocument/codeAction') then
-            -- Normal and Visual mode
-            vim.keymap.set({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-        end
-
         -- Rename
         if client:supports_method('textDocument/rename') then
             vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, opts)
-        end
-
-        -- Codelens
-        if client:supports_method('textDocument/codelens') then
-            vim.keymap.set({ 'n', 'x' }, '<leader>cl', function()
-                vim.lsp.codelens.run()
-            end, opts)
         end
 
         -- Formatting
