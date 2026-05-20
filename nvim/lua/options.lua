3e46074be700ecff07eaae0d76330b1b6c28227e
bryam <nunezb@uni-greifswald.de>
2026-05-20 22:10:24 +0200

    for c and cpp with cland

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -18,3 +18,13 @@ vim.opt.shiftwidth = 4     -- Number of spaces for each indentation
 vim.opt.expandtab = true   -- Use spaces instead of tabs
 vim.opt.smartindent = true -- Autoindent new lines
 vim.opt.autoindent = true  -- Copy indent from current line when starting a new one
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.smartindent = true
+    vim.opt_local.autoindent = true
+  end,
+})
