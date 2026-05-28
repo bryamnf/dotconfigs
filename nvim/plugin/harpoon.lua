6c37dcfa1f776bd44bd0d003dcae7a950595f318
bryam <nunezb@uni-greifswald.de>
2026-05-28 22:41:50 +0200

    redundant

--- nvim/plugin/harpoon.lua
+++ nvim/plugin/harpoon.lua
@@ -1,4 +1,4 @@
-vim.pack.add{
+vim.pack.add {
     { src = 'https://github.com/nvim-lua/plenary.nvim' },
     { src = 'https://github.com/ThePrimeagen/harpoon', version = 'harpoon2' },
 }
@@ -7,38 +7,6 @@ local harpoon = require("harpoon")
 -- REQUIRED
 harpoon:setup()
 -- REQUIRED
-
--- fzf-lua integration for harpoon
-local function toggle_fzf_harpoon()
-    local harpoon_files = harpoon:list()
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-
-    require("fzf-lua").fzf_exec(file_paths, {
-        prompt = "Harpoon> ",
-        preview = "bat --style=numbers --color=always {} || cat {}",
-        fzf_opts = {
-            ["--preview-window"] = "nohidden,right,50%",
-        },
-        actions = {
-            ["default"] = function(selected)
-                vim.cmd("edit " .. selected[1])
-            end,
-            ["ctrl-x"] = function(selected)
-                for i, item in ipairs(harpoon_files.items) do
-                    if item.value == selected[1] then
-                        harpoon:list():remove_at(i)
-                        break
-                    end
-                end
-            end,
-        },
-    })
-end
-vim.keymap.set("n", "<leader>h", toggle_fzf_harpoon)
-
 
 vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end)
