7b45bfead78ae0c3ad12e68892798c70a4bde29a
bryam <nunezb@uni-greifswald.de>
2026-09-02 12:20:02 +0200

    herdr config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotconfigs/configs/bash/bashrc.sh ~/.bashrc
 ln -sf ~/dotconfigs/configs/git/gitconfig ~/.gitconfig
 ln -sf ~/dotconfigs/configs/pi_agent/models.json ~/.pi/agent
 ln -sf ~/dotconfigs/configs/omp/models.yaml ~/.omp/agent
+ln -sf ~/dotconfigs/configs/herdr/config.toml ~/.config/herdr/config.toml
 
 ############## Config setup #################
 configs=("mise" "nvim" "tmux" "ghostty" "yazi" "opencode")
